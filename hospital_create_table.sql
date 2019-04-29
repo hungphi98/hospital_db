05511b5365e416662707ba14a94c3cd75b44ad9b
Phi H Nguyen <nguyenhungphi2698@Gmail.com>
2019-04-29 19:29:47 -0400

    Update hospital_create_table.sql

--- hospital_create_table.sql
+++ hospital_create_table.sql
@@ -1,25 +1,23 @@
-create table patient(p_id int, f_name varchar(30), l_name varchar(30), age int, dob DATE, address varchar(30), phone_number varchar(15), sex char(1), height numeric, weight numeric, primary key (p_id));
+create table patient(p_id serial, f_name varchar(30), l_name varchar(30), age int, dob DATE, address varchar(30), phone_number varchar(15), sex char(1), height numeric, weight numeric, primary key (p_id));
 
-create table staff(s_id int, f_name varchar(30), l_name varchar(30), address varchar(30), phone_number varchar(15), s_type varchar(10), d_id int, primary key(s_id));
+create table staff(s_id serial, f_name varchar(30), l_name varchar(30), address varchar(30), phone_number varchar(15), s_type varchar(10), d_id int REFERENCES hospital_department, username VARCHAR(32), password VARCHAR(32), primary key(s_id));
 
-create table hospital_department(d_id int, dept_name varchar(50), office_info varchar(50), visit_cost numeric, primary key(d_id));
+create table hospital_department(d_id serial, dept_name varchar(50), office_info varchar(50), visit_cost numeric, primary key(d_id));
 
 create table insurance_company(ip_id serial, name varchar(50), policy_id int, phone_number varchar(15), primary key(ip_id));
 
-create table insured_by(p_id int, ip_id int, primary key(ip_id));
+create table insured_by(p_id int REFERENCES patient, ip_id int REFERENCES insurance_company);
 
-create table procedures(pr_id int, name varchar(30), cost numeric, facility varchar(30), pr_type varchar(10), primary key(pr_id));
+create table procedures(pr_id serial, name varchar(30), cost numeric, facility varchar(30), per_hour char(1), primary key(pr_id));
 
-create table patient_history(ph_id int, p_id int, s_id int, pr_id int, start_time timestamp, end_time timestamp, description varchar(500), primary key(ph_id));
+create table patient_history(ph_id serial, p_id int REFERENCES patient, s_id int REFERENCES staff, pr_id int REFERENCES procedures, start_time timestamp, end_time timestamp, description varchar(500), primary key(ph_id));
 
-create table procedure_history(ph_id int, s_id int, primary key ());
+create table procedure_history(ph_id int REFERENCES patient_history, s_id int REFERENCES staff);
 
-create table ward(p_id int, d_id int, arrival timestamp, departure timestamp, cost_per_day numeric);
+create table ward(p_id int REFERENCES patient, d_id int REFERENCES hospital_department, arrival timestamp, departure timestamp, cost_per_day numeric);
 
 create table medications(m_id serial, name varchar(30), purpose varchar(100), cost numeric, primary key(m_id));
 
-create table prescribed(ph_id int, m_id int, dosage varchar(15), primary key());
+create table prescribed(ph_id int REFERENCES patient_history, m_id int REFERENCES medications, dosage varchar(15));
 
-create table bills(p_id int, b_id serial, start_date timestamp, end_date timestamp, total_cost numeric, total_paid numeric, primary key (b_id));
-
-create table users(username VARCHAR(32), password VARCHAR(32), s_id serial, PRIMARY KEY(username));
+create table bills(b_id serial, p_id int REFERENCES patient, start_date timestamp, end_date timestamp, total_cost numeric, total_paid numeric, primary key (b_id));
