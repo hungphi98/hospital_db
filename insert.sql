700dbc1610847987f2daa14e825dbcb6ab26f662
jordanchristian <jachris15@earlham.edu>
2019-04-30 03:28:32 -0400

    All insert statements except for Bills

--- insert.sql
+++ insert.sql
@@ -1,9 +1,10 @@
 INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('John', 'Doe', 27, '1990-01-01', '801 National R W', '765-277-0909', 'M', 1.88, 70.3);
+INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Sara', 'Doe', 21, '1985-05-31', '10 West Main St', '765-977-2324', 'F', 1.5, 200);
+INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Roger', 'Jack', 50, '1969-09-13', '801 National R W', '765-277-0909', 'M', 1.88, 215.0);
 INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Elizabeth', 'Smith', 15, '2004-07-27', '517 N 5th St', '765-277-1514', 'F', 1.3, 90.3);
 INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Zach', 'Taylor', 69, '1950-01-01', '20 S Blvd', '765-962-7564', 'M', 1.9, 100);
 INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Susan', 'Anthony', 3, '2016-01-01', '9 N 9th', '765-914-2775', 'F', 0.5, 20);
 INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('Bill', 'Radford', 80, '1939-01-01', '1755 West Main St', '765-855-2309', 'M', 2.1, 150);
-
 
 INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('cardiology', 'CST217', 50.0);
 INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('Acute Rehab', '4th Floor', 100.0);
@@ -15,17 +16,34 @@ INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('Nu
 INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('Outpatient', '1st Floor', 0.0);
 INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('Orthopedics', '1st Floor', 100.0);
 
-
-
-INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Alice', 'White', '120 Broadway', '911', 'admin',1, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Alice', 'White', '120 Broadway', '765-977-2334', 'admin',1, 'admin', 'admin');
 INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Christian', 'Wilkins', '15 South 20th', '765-962-5419', 'admin',2, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('James', 'Thompson', '801 National Rd W', '765-932-6755', 'admin',3, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Susan', 'Jones', '100 South 9th', '765-962-9878', 'admin',4, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Bob', 'Wilks', '1 W 20th', '765-567-1234', 'admin',5, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Raven', 'McGee', '215 SW 5th', '765-444-4545', 'admin',6, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Cassidy', 'Fulmer', '2100 Chester Blvd', '317-962-5419', 'admin',7, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Alyssa', 'Hand', '987 SW 2nd', '937-555-5999', 'admin',8, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('John', 'Greve', '128 West Main St', '765-087-3222', 'admin',9, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Daniel', 'Jones', '2100 South 20th, Apt 5', '765-902-5419', 'admin',10, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Charlissa', 'Fill', '8976 Sunset Blvd', '317-991-5419', 'Doctor',1, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Xavier', 'Rhodes', '345 Hello Drive', '765-914-9223', 'Doctor',2, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Susan', 'Lamer', '15 S Pkwy', '765-265-8876', 'Doctor',3, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Kim', 'Crossing', '15 New Lane', '765-338-7734', 'Doctor',4, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Becky', 'Jenkins', '15 Chester Blvd', '765-855-2120', 'Doctor',5, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Morgan', 'Jones', '17 NW 5th', '765-912-3030', 'Doctor',6, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Christina', 'Peck', '2100 S 18th', '765-0011-3333', 'Doctor',7, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Wilson', 'Hernandez', '15 E 10th', '765-209-4545', 'Doctor',8, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Brian', 'Shaffer', '5 NW 20th', '765-238-9090', 'Doctor',9, 'admin', 'admin');
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Krystal', 'Hockersmith', '215 SW 5th', '765-444-4545', 'Doctor',10, 'admin', 'admin');
+
 
 INSERT into insurance_company (name, policy_id, phone_number) VALUES ('Medicaid', '12345', '812-567-2859');
 INSERT into insurance_company (name, policy_id, phone_number) VALUES ('Medicare', '72752', '587-999-2859');
 INSERT into insurance_company (name, policy_id, phone_number) VALUES ('Health For All', '10116', '342-901-9959');
 INSERT into insurance_company (name, policy_id, phone_number) VALUES ('Wellness Mutual', '67548', '765-987-1423');
 
-INSERT into procedures (name, cost, facility, per_hour) VALUES ('MRI', 500.0, 'Outpatient', 'N');
+INSERT into procedures (name, cost, facility, per_hour) VALUES ('MRI', 500.0, 'Outpatient', 'N');  /*select p_id, pr_id, name, cost fom proceedures natural join patient_history where p_id =2; */
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('Cat Scan', 1000.0, 'Outpatient', 'N');
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('X-Ray', 500.0, 'Outpatient', 'N');
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('Appendectomy', 250.0, 'Emergency Room', 'Y');
@@ -38,6 +56,37 @@ INSERT into procedures (name, cost, facility, per_hour) VALUES ('C-Section', 100
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('Natural Birth', 500.0, 'Mother Baby', 'Y');
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('Liver Transplant', 2500.0, 'Outpatient', 'Y');
 INSERT into procedures (name, cost, facility, per_hour) VALUES ('Kidney Transplant', 3000.0, 'Outpatient', 'Y');
+INSERT into procedures (name, cost, facility, per_hour) VALUES ('Burn Care', 200.0, 'Burn Center', 'N');
+
+
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (1, 17, 8, '2019-05-04 10:00:00', '2019-05-04 12:00:00', 'This patient had ACL reconstructive surgery in their left knee. Surgery was a success');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (2, 8, 1, '2019-01-08 9:00:00', '2019-05-04 10:00:00', 'This patient had an MRI on her left wrist after falling during cheerleading');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (4, 13, 4, '2018-12-04 13:00:00', '2018-12-04 15:00:00', 'This patient needed emergency surgery for the removal of her appendix');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (5, 18, 13, '2019-05-04 10:00:00', '2019-05-04 12:00:00', 'This patient received a Kidney Transplant');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (6, 4, 3, '2017-04-19 09:00:00', '2017-04-04 10:00:00', '3-year old Susan came to the Emergency Room for an X-Ray on her leg after falling outside. X-Ray was negative');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (7, 11, 14, '2019-03-09 15:00:00', '2017-04-04 15:30:00', 'Patient came in to treat minor burnes suffered from cooking');
+insert into patient_history (p_id, s_id, pr_id, start_time, end_time, description) values (3, 10, 7, '2018-08-21 09:00:00', '2018-08-21 14:00:00', 'Patient had Triple Bypass surgery to fix blocked artery. Surgery was successful.');
+
+
+INSERT into procedure_history (ph_id, s_id) values (1,17);
+INSERT into procedure_history (ph_id, s_id) values (2,8);
+INSERT into procedure_history (ph_id, s_id) values (3,13);
+INSERT into procedure_history (ph_id, s_id) values (4,18);
+INSERT into procedure_history (ph_id, s_id) values (5,4);
+INSERT into procedure_history (ph_id, s_id) values (6,11);
+INSERT into procedure_history (ph_id, s_id) values (7,10);
+
+INSERT into ward (p_id, d_id, arrival, departure, cost_per_day) VALUES (1, 10, '2019-05-04 13:00:00', '2019-05-11 12:00:00', 100); /*Add columns days_spent, total_cost (days_spent * cost_per_day)*/
+INSERT into ward (p_id, d_id, arrival, departure, cost_per_day) VALUES (4, 9, '2018-12-04 16:00:00', '2018-12-04 12:00:00', 200);
+INSERT into ward (p_id, d_id, arrival, departure, cost_per_day) VALUES (5, 9, '2019-05-04 13:00:00', '2019-05-11 12:00:00', 200);
+INSERT into ward (p_id, d_id, arrival, departure, cost_per_day) VALUES (3, 1, '2018-08-21 15:00:00', '2018-09-15 12:00:00', 50);
+
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (1, 12, '1 Tab/Day');  /*Possibly add p_id to map patient to medicine to make it easier to sum cost*/
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (2, 11, 'As needed');   /* select * from medications natural join prescribed this will give eveything we need for medicine cost */
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (3, 3, '1 Tab/Day');
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (4, 8, '1 Tabs Daily');
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (6, 4, '1 Tab/Day');
+INSERT into prescribed (ph_id, m_id, dosage) VALUES (7, 5, '1 Tab/Day');
 
 INSERT into medications (name, purpose, cost) VALUES ('Depakote', 'Anti-Epileptic drug to prevent seizures', 100.0);
 INSERT into medications (name, purpose, cost) VALUES ('Keppra', 'Anti-Epileptic drug to prevent seizures', 200.0);
