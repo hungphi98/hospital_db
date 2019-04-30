196e3777e526357342763f723e3941dc828b711d
hungphi98 <nguyenhungphi2698@gmail.com>
2019-04-29 20:02:22 -0400

    insert into db

---
+++ insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO patient(f_name, l_name, age, dob, address, phone_number, sex, height, weight) VALUES('John', 'Doe', 27, '1990-01-01', '801 National R W', '765-277-0909', 'M', 1.88, 70.3);
+
+INSERT INTO hospital_department (dept_name, office_info, visit_cost) VALUES ('cardiology', 'CST217', 50.0);
+
+INSERT INTO staff (f_name, l_name, address, phone_number, s_type, d_id, username, password) VALUES ('Alice', 'White', '120 Broadway', '911', 'admin',1, 'admin', 'admin');
+
